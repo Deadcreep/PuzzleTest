2566500772d8778054e43d544609cb6c3851bed0
Dayan <unity3@ceramic3d.ru>
2022-10-08 19:40:50 +0500

    Update WinCondition.cs

--- Assets/Scripts/WinConditions/WinCondition.cs
+++ Assets/Scripts/WinConditions/WinCondition.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 public enum WinStatus
 {
@@ -11,7 +6,6 @@ public enum WinStatus
 	Fail,
 	InProgress
 }
-
 
 public abstract class WinCondition : MonoBehaviour
 {
